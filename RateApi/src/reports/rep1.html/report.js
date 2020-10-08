f568e26f9afdf07d544dd7b48c8a5836b1c98a5a
bhavana.saragadam <bhavana.saragadam@HYDPCM344549L.ad.infosys.com>
2020-10-08 20:57:54 +0530

    reports commit

--- RateApi/src/reports/rep1.html/report.js
+++ RateApi/src/reports/rep1.html/report.js
@@ -16,38 +16,38 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Latest Foreign Exchange rates",
+  "name": "Rates API for Latest Foreign Exchange rates-1",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "The API is available",
+  "name": "The API is available-1",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "verify the success status of the response",
+  "name": "verify the success status of the response-1",
   "keyword": "Then "
 });
 formatter.match({
   "location": "stepdefinition1.rates_API_for_Latest_Foreign_Exchange_rates()"
 });
 formatter.result({
-  "duration": 5984583900,
+  "duration": 3583524000,
   "status": "passed"
 });
 formatter.match({
   "location": "stepdefinition1.the_API_is_available()"
 });
 formatter.result({
-  "duration": 316700,
+  "duration": 213400,
   "status": "passed"
 });
 formatter.match({
   "location": "stepdefinition1.verify_the_success_status_of_the_response()"
 });
 formatter.result({
-  "duration": 5770300,
+  "duration": 4177100,
   "status": "passed"
 });
 formatter.uri("Scenario2.feature");
@@ -68,36 +68,39 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Latest Foreign Exchange rates",
+  "name": "Rates API for Latest Foreign Exchange rates-2",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "The API is available",
+  "name": "The API is available-2",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "verify the response",
+  "name": "verify the response-2",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "stepdefinition1.rates_API_for_Latest_Foreign_Exchange_rates()"
+  "location": "stepdefinition2.rates_API_for_Latest_Foreign_Exchange_rates()"
 });
 formatter.result({
-  "duration": 994607300,
+  "duration": 926884900,
   "status": "passed"
 });
 formatter.match({
-  "location": "stepdefinition1.the_API_is_available()"
+  "location": "stepdefinition2.the_API_is_available()"
 });
 formatter.result({
-  "duration": 86700,
+  "duration": 70300,
   "status": "passed"
 });
-formatter.match({});
+formatter.match({
+  "location": "stepdefinition2.verify_the_response()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 16694000,
+  "status": "passed"
 });
 formatter.uri("Scenario3.feature");
 formatter.feature({
@@ -117,7 +120,7 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Latest Foreign Exchange rates",
+  "name": "Rates API for Latest Foreign Exchange rates-3",
   "keyword": "Given "
 });
 formatter.step({
@@ -127,23 +130,29 @@ formatter.step({
 });
 formatter.step({
   "line": 7,
-  "name": "validate the correct response supplied by the call",
+  "name": "validate the correct response supplied by the call-3",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "stepdefinition1.rates_API_for_Latest_Foreign_Exchange_rates()"
+  "location": "stepdefinition3.rates_API_for_Latest_Foreign_Exchange_rates()"
 });
 formatter.result({
-  "duration": 925789700,
+  "duration": 937027800,
   "status": "passed"
 });
-formatter.match({});
-formatter.result({
-  "status": "undefined"
+formatter.match({
+  "location": "stepdefinition3.an_incorrect_or_incomplete_url_is_provided_e_g_https_api_ratesapi_io_api()"
 });
-formatter.match({});
 formatter.result({
-  "status": "undefined"
+  "duration": 1418343400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition3.validate_the_correct_response_supplied_by_the_call()"
+});
+formatter.result({
+  "duration": 828800,
+  "status": "passed"
 });
 formatter.uri("Scenario4.feature");
 formatter.feature({
@@ -163,34 +172,39 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Specific date Foreign Exchange rates",
+  "name": "Rates API for Specific date Foreign Exchange rates-4",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "The API is available",
+  "name": "The API is available-4",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "verify the success status of the response",
+  "name": "verify the success status of the response-4",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "stepdefinition4.rates_API_for_Specific_date_Foreign_Exchange_rates()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 801521500,
+  "status": "passed"
 });
 formatter.match({
-  "location": "stepdefinition1.the_API_is_available()"
+  "location": "stepdefinition4.the_API_is_available()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 89700,
+  "status": "passed"
 });
 formatter.match({
-  "location": "stepdefinition1.verify_the_success_status_of_the_response()"
+  "location": "stepdefinition4.verify_the_success_status_of_the_response()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 257700,
+  "status": "passed"
 });
 formatter.uri("Scenario5.feature");
 formatter.feature({
@@ -210,32 +224,39 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Specific date Foreign Exchange rates",
+  "name": "Rates API for Specific date Foreign Exchange rates-5",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "The API is available",
+  "name": "The API is available-5",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "verify the response",
+  "name": "verify the response-5",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "stepdefinition5.rates_API_for_Specific_date_Foreign_Exchange_rates()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 663638300,
+  "status": "passed"
 });
 formatter.match({
-  "location": "stepdefinition1.the_API_is_available()"
+  "location": "stepdefinition5.the_API_is_available()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 71000,
+  "status": "passed"
 });
-formatter.match({});
+formatter.match({
+  "location": "stepdefinition5.verify_the_response()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 388400,
+  "status": "passed"
 });
 formatter.uri("Scenario6.feature");
 formatter.feature({
@@ -255,29 +276,38 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "Rates API for Specific date Foreign Exchange rates",
+  "name": "Rates API for Specific date Foreign Exchange rates-6",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "An incorrect or incomplete url is provided e.g: https://api.ratesapi.io/api/",
+  "name": "A future date is provided in the url",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "validate the correct response supplied by the call",
+  "name": "validate the correct response supplied by the call-6",
   "keyword": "Then "
 });
-formatter.match({});
-formatter.result({
-  "status": "undefined"
+formatter.match({
+  "location": "stepdefinition6.rates_API_for_Specific_date_Foreign_Exchange_rates()"
 });
-formatter.match({});
 formatter.result({
-  "status": "undefined"
+  "duration": 639199800,
+  "status": "passed"
 });
-formatter.match({});
+formatter.match({
+  "location": "stepdefinition6.a_future_date_is_provided_in_the_url()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 922046000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepdefinition6.validate_the_correct_response_supplied_by_the_call()"
+});
+formatter.result({
+  "duration": 560600,
+  "status": "passed"
 });
 });
